07c188dde895d89c59519c9c7f53a73c68ca162d
leeyc924 <leeyc924@naver.com>
2023-11-18 21:11:02 +0900

    Modify user

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,23 +1,26 @@
 export const TIER_COLOR: Record<number, { border: string; background: string }> = {
-  0: {
+  1: {
     border: 'border-violet-500',
     background: 'bg-violet-500',
   },
-  1: {
+  2: {
     border: 'border-blue-500',
     background: 'bg-blue-500',
   },
-  2: {
+  3: {
     border: 'border-emerald-500',
     background: 'bg-emerald-500',
   },
-  3: {
+  4: {
     border: 'border-yellow-500',
     background: 'bg-yellow-500',
   },
-  4: {
+  5: {
     border: 'border-pink-400',
     background: 'bg-pink-400',
   },
+  99: {
+    border: 'border-gray-300',
+    background: 'bg-gray-300',
+  },
 };
-export const MAX_TIER = 4;
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8000,
+  },
 });
