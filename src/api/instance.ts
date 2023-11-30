4a0284be81a2934b6a5a45dfce244a5cc25ca8a6
이영철 <leeyc@jobkorea.co.kr>
2023-11-30 10:16:36 +0900

    Add log

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -7,6 +7,8 @@ interface RequestConfig extends AxiosRequestConfig {
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 function createAxios(requestConfig: RequestConfig): AxiosInstance {
+  console.log(baseURL, 'baseURL');
+  console.log(`import.meta.env`, import.meta.env);
   const axiosInstance = axios.create({
     baseURL: requestConfig.baseURL,
     headers: {
