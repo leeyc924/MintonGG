9f48107ed6e4dd7d52ee5514a4b3471a71840a8a
leeyc924 <leeyc924@naver.com>
2024-01-23 23:34:17 +0900

    Modify text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     <div>
       <Typography>안녕하세요 동호회 유저 매니저 입니다</Typography>
       <Logout />
-      <Link href="/privacy">개인정보처리방침</Link>
+      <Link href="/privacy">
+        <Typography variant="caption">개인정보처리방침</Typography>
+      </Link>
     </div>
   );
 }
