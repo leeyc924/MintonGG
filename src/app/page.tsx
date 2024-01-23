9407184bc50d02b512d9013e113dd84d97e67a96
leeyc924 <leeyc924@naver.com>
2024-01-23 23:09:20 +0900

    Add privacy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Typography from '@mui/material/Typography';
+import Link from 'next/link';
 import Logout from './logout';
 
 export default function Home() {
   return (
     <div>
-      <Typography>안녕하세요 Minton GG 입니다</Typography>
+      <Typography>안녕하세요 동호회 유저 매니저 입니다</Typography>
       <Logout />
+      <Link href="/privacy">개인정보처리방침</Link>
     </div>
   );
 }
